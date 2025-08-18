d5fa7f1ce615ad7f28e4f53e73d23393db61e10f
JULIA GABRIELI GARCIA DA SILVA <julia_gg_silva@estudante.sesisenai.org.br>
2025-08-18 19:58:17 -0300

    Criando o MenuPrincipal na view

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,4 +1,21 @@
 package view;
 
+import java.util.Scanner;
+
 public class Menu {
+    Scanner sc = new Scanner(System.in);
+    public void menuPrincipal(){
+        int opcao = 0;
+        do{
+            System.out.println("1. Criar tarefa");
+            System.out.println("2. Listar tarefas");
+            System.out.println("3. Marcar tarefa como concluída");
+            System.out.println("4. Listar tarefas vencidas");
+            System.out.println("5. Listar tarefa por prioridade");
+            System.out.println("0. Sair");
+            opcao = sc.nextInt();
+        }
+        while(opcao !=0);
+    }
+
 }
